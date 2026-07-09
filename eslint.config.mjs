d0941bc2bcdb93c6b570a5538926156251b40d18
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-07-09 09:27:47 -0300

    chore(Prettier): instalado prettier para formatação de arquivos Jira: AMC-385

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default withNuxt(
   // Your custom configs here
+).append(
+  // Must be appended last to turn off formatting rules conflicting with Prettier
+  eslintConfigPrettier,
 );
