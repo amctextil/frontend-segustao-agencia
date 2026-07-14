d1006b71b570a3c4ddba3f73db0ef3dbf7c55e68
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-07-14 09:45:49 -0300

    incluído logo e ajuste na tela de login

---
+++ app/layouts/login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { WDColors } from 'widelab-utils';
+
+const configStore = useConfigStore();
+</script>
+
+<template>
+  <div
+    class="flex h-screen w-screen flex-col md:flex-row overflow-hidden bg-slate-50"
+  >
+    <div
+      class="hidden md:flex w-1/2 items-center justify-center"
+      :style="`background-color: ${configStore.selectedBrand?.corApp} !important;`"
+    >
+      <NuxtImg
+        v-if="configStore.selectedBrand?.imagemAppLogin"
+        :src="configStore.selectedBrand?.imagemAppLogin"
+        width="300"
+      />
+      <AMCLogo
+        v-else
+        :fill="
+          WDColors.getContrastingTextColor(
+            configStore.selectedBrand?.corApp || '#FFFFFF',
+          )
+        "
+        width="300"
+      />
+    </div>
+
+    <div class="flex w-1/2 items-center justify-center">
+      <slot />
+    </div>
+  </div>
+</template>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { BRAND_LIST } from '~/constants/config';
+
+const configStore = useConfigStore();
 const { loggedIn, user, fetch: refreshSession } = useUserSession();
+
 const credentials = reactive({
   email: '',
   password: '',
+  brand: configStore.selectedBrand ? configStore.selectedBrand.appId : '',
 });
 async function login() {
   try {
@@ -18,16 +23,46 @@ async function login() {
     alert('Bad credentials');
   }
 }
+
+const DropDownList = BRAND_LIST.map(({ title, value }) => ({
+  label: title,
+  value: value as string,
+}));
 </script>
 
 <template>
-  <form @submit.prevent="login">
-    <input v-model="credentials.email" type="email" placeholder="Email" />
-    <input
-      v-model="credentials.password"
-      type="password"
-      placeholder="Password"
-    />
-    <button type="submit">Login</button>
-  </form>
+  <NuxtLayout name="login">
+    <div
+      class="m-8 md:w-2/3 flex flex-col items-center justify-center gap-8 text-gray-700"
+    >
+      <h1 class="self-start text-2xl font-semibold">Login</h1>
+
+      <form
+        class="w-full flex flex-col gap-8 text-gray-800"
+        @submit.prevent="login"
+      >
+        <UFormField label="Marca" name="brand">
+          <USelect
+            v-model="credentials.brand"
+            class="w-full"
+            :items="DropDownList"
+          />
+        </UFormField>
+
+        <UFormField label="Email" name="email">
+          <UInput v-model="credentials.email" class="w-full" />
+        </UFormField>
+
+        <UFormField label="Password" name="password">
+          <UInput
+            v-model="credentials.password"
+            type="password"
+            class="w-full"
+          />
+        </UFormField>
+
+        <UButton type="submit" color="success"> Entrar </UButton>
+      </form>
+    </div>
+  </NuxtLayout>
 </template>
