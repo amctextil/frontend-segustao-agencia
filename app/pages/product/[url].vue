97ddd42d4847bc3bbddd5894674911dd1afb8360
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-07-16 17:50:35 -0300

    feat(Home): incluído tela de detalhes do produto Jira: AMC-401

--- app/pages/product/[url].vue
+++ app/pages/product/[url].vue
@@ -1,12 +1,47 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
   <NuxtLayout name="main">
-    <h1>detalhes do produto</h1>
+    <div class="d-flex pa-4 ga-4 flex-column overflow-auto">
+      <div class="d-flex ga-4">
+        <v-carousel>
+          <v-carousel-item
+            v-for="media in medias"
+            :key="media.MediaPath"
+            :src="'https://d3vnyi5j6ba1mc.cloudfront.net' + media.MediaPath"
+          />
+        </v-carousel>
 
-    <span>{{ product?.Name }}</span>
+        <div class="d-flex flex-column ga-4">
+          <h1 class="text-display-small">{{ product.Name }}</h1>
+
+          <span>Ref: {{ product.IntegrationID }}</span>
+
+          <span
+            v-if="product.PromotionPrice"
+            class="text-grey text-decoration-line-through"
+          >
+            {{ WDFormatters.formatCurrency(product.ListPrice) }}
+          </span>
+
+          <span class="text-display-small font-weight-bold">
+            {{
+              product.PromotionPrice
+                ? WDFormatters.formatCurrency(product.PromotionPrice)
+                : WDFormatters.formatCurrency(product.ListPrice)
+            }}
+          </span>
+
+          <v-btn> Adicionar </v-btn>
+        </div>
+      </div>
+
+      <div v-for="desc in descriptions" :key="desc.Alias" v-html="desc.Value" />
+    </div>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
+import { WDFormatters } from 'widelab-utils';
 import { ProductService } from '~/services/product.service';
 
 const route = useRoute();
@@ -18,7 +53,36 @@ const product = await ProductService.getProductByUrl(
   productUrl,
   user.value!.appId,
 );
-console.log('🚀 ~ product:', product);
+
+if (!product) {
+  throw createError({
+    statusCode: 500,
+    statusMessage: 'Produto não encontrado',
+  });
+}
+
+const options = ref<{ [title: string]: string }>({});
+
+const medias = computed(() => {
+  const allMedias = product.Medias.filter(
+    (media) => media.MediaSizeType === 'Medium',
+  );
+
+  if (!options.value.Cor) {
+    return allMedias;
+  }
+
+  const variantMedias = allMedias?.filter(
+    (media) =>
+      !!(options.value.Cor && media.VariationPath.includes(options.value.Cor)),
+  );
+
+  return variantMedias?.length ? variantMedias : allMedias;
+});
+
+const descriptions = product.Descriptions.filter((item) => item.Value);
+
+console.log('🚀 ~ product.Descriptions:', product.Descriptions);
 </script>
 
 <style></style>
