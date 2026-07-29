38c95181718de5a7dd5c54b6aae2ec1068a8cad2
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-07-29 18:07:50 -0300

    feat(Início): desenvolvendo modal de busca de produtos Jira: AMC-417

---
+++ app/components/modal-search-products.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-bottom-sheet v-model="isActive">
+    <v-sheet>
+      <h1>Producar produtos</h1>
+    </v-sheet>
+  </v-bottom-sheet>
+</template>
+
+<script lang="ts" setup>
+const isActive = defineModel<boolean>();
+</script>
+
+<style></style>
