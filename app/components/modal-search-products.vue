57340b6cb54ec21c79296d0a86646abd181bc0e3
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-07-30 15:27:51 -0300

    feat(Produtos): incluído pesquisa de produtos na API Jira: AMC-417

--- app/components/modal-search-products.vue
+++ app/components/modal-search-products.vue
@@ -1,13 +1,70 @@
 <template>
   <v-bottom-sheet v-model="isActive">
-    <v-sheet>
-      <h1>Producar produtos</h1>
-    </v-sheet>
+    <v-card title="Procurar produtos">
+      <template #append>
+        <v-btn icon="mdi-close" variant="flat" @click="isActive = false" />
+      </template>
+
+      <div class="d-flex flex-column ga-4 px-8">
+        <v-text-field
+          v-model="search"
+          prepend-inner-icon="mdi-magnify"
+          :loading="status === 'pending'"
+        />
+
+        <ul class="d-flex flex-row pa-4 pt-0 ga-4 flex-wrap ma-0 overflow-auto">
+          <NuxtLink
+            v-for="item in data?.Products"
+            :key="item.ProductID"
+            :to="`/produto${item.Url}`"
+            class="text-decoration-none text-black"
+          >
+            <ProductGridItem :model-value="item" class="flex-fill" />
+          </NuxtLink>
+        </ul>
+      </div>
+    </v-card>
   </v-bottom-sheet>
 </template>
 
 <script lang="ts" setup>
+import { ProductService } from '~/services/product.service';
+
 const isActive = defineModel<boolean>();
+const search = ref('');
+const debouncedQuery = ref('');
+let timeoutId: NodeJS.Timeout;
+
+const { user } = useUserSession();
+
+watch([search], () => {
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    debouncedQuery.value = search.value;
+  }, 300); // 300ms delay
+});
+
+const { data, status } = await useAsyncData(
+  'search-results',
+  async () => {
+    if (!debouncedQuery.value) {
+      return await ProductService.getByList(user.value!.appId, 1, 'roupas');
+    }
+
+    const { data } = await ProductService.getBySearch(
+      debouncedQuery.value,
+      user.value!.appId,
+      1,
+    );
+
+    return data;
+  },
+  {
+    watch: [debouncedQuery],
+    immediate: true,
+  },
+);
 </script>
 
 <style></style>
