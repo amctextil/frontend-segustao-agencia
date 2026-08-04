13e5fd1c02d248b8758a4d9f0cb2d75affd50ece
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-04 12:06:13 -0300

    feat(Início): iniciado desenvolvimento do modal de filtros Jira: AMC-417

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,15 +8,19 @@ const list = await ProductService.getByList(user.value!.appId, 1, 'roupas');
 
 <template>
   <NuxtLayout name="main">
-    <ul class="d-flex flex-row pa-4 pt-0 ga-4 flex-wrap ma-0 overflow-auto">
-      <NuxtLink
-        v-for="item in list.Products"
-        :key="item.ProductID"
-        :to="`/produto${item.Url}`"
-        class="text-decoration-none text-black"
-      >
-        <ProductGridItem :model-value="item" class="flex-fill" />
-      </NuxtLink>
-    </ul>
+    <div class="d-flex flex-column px-4 ga-4 ma-0 align-start">
+      <v-btn>Filtros</v-btn>
+
+      <ul class="d-flex flex-row pa-0 ga-4 flex-wrap ma-0 overflow-auto">
+        <NuxtLink
+          v-for="item in list.Products"
+          :key="item.ProductID"
+          :to="`/produto${item.Url}`"
+          class="text-decoration-none text-black"
+        >
+          <ProductGridItem :model-value="item" class="flex-fill" />
+        </NuxtLink>
+      </ul>
+    </div>
   </NuxtLayout>
 </template>
