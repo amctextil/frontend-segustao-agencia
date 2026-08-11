ad28dbf80190a2d16f64214a52c13b0ea96c259e
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-11 18:05:56 -0300

    feat(Home): separado modal de filtros em componente próprio Jira: AMC-417

---
+++ app/components/filters-modal.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-row>
+    <v-btn @click="filterModal = !filterModal">Filtros</v-btn>
+
+    <v-chip v-if="!!selectedCategory" closable @click:close="clear">
+      {{
+        categories?.find((item) => item.url === selectedCategory)?.categoria ||
+        selectedCategory
+      }}
+    </v-chip>
+  </v-row>
+
+  <v-navigation-drawer v-model="filterModal" temporary :persistent="isLoading">
+    <template #prepend>
+      <v-list>
+        <v-list-item title="Selecionar Categoria" />
+      </v-list>
+      <v-divider />
+    </template>
+
+    <v-list>
+      <v-list-item
+        v-for="category in categories"
+        :key="category.id"
+        :title="category.categoria"
+        :active="selectedCategory === category.url"
+        @click="isLoading ? undefined : (selectedCategory = category.url)"
+      />
+    </v-list>
+
+    <template #append>
+      <v-divider />
+
+      <div class="d-flex flex-column ga-4 pa-2">
+        <v-btn :loading="isLoading" variant="text" @click="clear">Limpar</v-btn>
+        <v-btn :loading="isLoading" @click="filter">Buscar</v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts" setup>
+import { ProductService } from '~/services/product.service';
+
+const { user } = useUserSession();
+
+const categories = await ProductService.getCategories(user.value!.appId);
+
+const emit = defineEmits<{
+  (
+    event: 'search',
+    payload: { category: string; resolve: (value: undefined) => void },
+  ): Promise<void>;
+}>();
+
+const selectedCategory = ref('');
+const isLoading = ref(false);
+const filterModal = ref(false);
+
+const clear = () => {
+  selectedCategory.value = '';
+  filter();
+};
+
+const filter = async () => {
+  isLoading.value = true;
+  try {
+    const category = selectedCategory.value.replace(/\/?roupas\/?/g, '');
+
+    await new Promise((resolve) => {
+      emit('search', { category, resolve });
+    });
+
+    filterModal.value = false;
+  } finally {
+    isLoading.value = false;
+  }
+};
+</script>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,81 +6,28 @@ const { user } = useUserSession();
 const list = ref(
   await ProductService.getByList(user.value!.appId, 1, 'roupas/'),
 );
-const categories = await ProductService.getCategories(user.value!.appId);
 
-const filterModal = ref(false);
-const selectedCategory = ref('');
-const isLoading = ref(false);
+const searchProducts = async ({
+  category,
+  resolve,
+}: {
+  category: string;
+  resolve: (value?: undefined) => void;
+}) => {
+  list.value = await ProductService.getByList(
+    user.value!.appId,
+    1,
+    'roupas/' + category,
+  );
 
-const clear = () => {
-  selectedCategory.value = '';
-  filter();
-};
-
-const filter = async () => {
-  isLoading.value = true;
-  try {
-    const category = selectedCategory.value.replace(/\/?roupas\/?/g, '');
-
-    list.value = await ProductService.getByList(
-      user.value!.appId,
-      1,
-      'roupas/' + category,
-    );
-    filterModal.value = false;
-  } finally {
-    isLoading.value = false;
-  }
+  resolve();
 };
 </script>
 
 <template>
   <NuxtLayout name="main">
     <div class="d-flex flex-column px-4 ga-4 ma-0 align-start overflow-hidden">
-      <v-row>
-        <v-btn @click="filterModal = !filterModal">Filtros</v-btn>
-
-        <v-chip v-if="!!selectedCategory" closable @click:close="clear">
-          {{
-            categories?.find((item) => item.url === selectedCategory)
-              ?.categoria || selectedCategory
-          }}
-        </v-chip>
-      </v-row>
-
-      <v-navigation-drawer
-        v-model="filterModal"
-        temporary
-        :persistent="isLoading"
-      >
-        <template #prepend>
-          <v-list>
-            <v-list-item title="Selecionar Categoria" />
-          </v-list>
-          <v-divider />
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="category in categories"
-            :key="category.id"
-            :title="category.categoria"
-            :active="selectedCategory === category.url"
-            @click="isLoading ? undefined : (selectedCategory = category.url)"
-          />
-        </v-list>
-
-        <template #append>
-          <v-divider />
-
-          <div class="d-flex flex-column ga-4 pa-2">
-            <v-btn :loading="isLoading" variant="text" @click="clear"
-              >Limpar</v-btn
-            >
-            <v-btn :loading="isLoading" @click="filter">Buscar</v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
+      <filters-modal @search="searchProducts" />
 
       <ul class="d-flex flex-row pa-0 ga-4 flex-wrap ma-0 overflow-auto">
         <NuxtLink
