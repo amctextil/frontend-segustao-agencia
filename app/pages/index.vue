e35b6da88079aa7797220b785967876b03dd2a0a
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-14 10:20:30 -0300

    fix(Home): ajuste no loading dos produtos Jira: AMC-417

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -51,13 +51,16 @@ watch(
     <div class="d-flex flex-column px-4 ga-4 ma-0 align-start overflow-hidden">
       <filters-modal @search="searchProducts" />
 
-      <div v-if="!list?.Products && !isLoading">
-        <h1>Erro ao buscar produtos</h1>
-      </div>
       <ul
-        v-if="!isLoading"
-        class="d-flex flex-row pa-0 ga-4 flex-wrap ma-0 overflow-auto"
+        v-if="isLoading"
+        class="d-flex flex-row pa-0 ga-4 flex-wrap ma-0 overflow-hidden"
       >
+        <v-skeleton-loader v-for="n in 40" :key="n" width="200" height="300" />
+      </ul>
+      <div v-else-if="!list?.Products.length" class="pa-4">
+        <h2>nenhum produto cadastrado</h2>
+      </div>
+      <ul v-else class="d-flex flex-row pa-0 ga-4 flex-wrap ma-0 overflow-auto">
         <NuxtLink
           v-for="item in list?.Products"
           :key="item.ProductID"
@@ -66,9 +69,6 @@ watch(
         >
           <ProductGridItem :model-value="item" class="flex-fill" />
         </NuxtLink>
-      </ul>
-      <ul v-else class="d-flex flex-row pa-0 ga-4 flex-wrap ma-0 overflow-auto">
-        <v-skeleton-loader v-for="n in 10" :key="n" width="200" height="300" />
       </ul>
     </div>
   </NuxtLayout>
