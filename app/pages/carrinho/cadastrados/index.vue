d3ac7e1c314bb3be8c2e7fb00e51d5b5391ce882
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-14 14:59:38 -0300

    feat(Carrinhos): incluído componente para os carrinhos cadstrados Jira: AMC-417

--- app/pages/carrinho/cadastrados/index.vue
+++ app/pages/carrinho/cadastrados/index.vue
@@ -10,45 +10,12 @@
       <h2>Não há nenhum carrinho cadastrado para esta marca</h2>
     </div>
     <div v-else class="d-flex flex-row flex-wrap ga-4 pa-4 overflow-auto">
-      <v-card
-        v-for="cart in list"
-        :key="cart.id"
-        width="250"
-        :subtitle="WDDates.toString(new Date(cart.criadoEm), 'BR', true)"
-        :to="`/carrinho/cadastrados/${cart.id}`"
-        rounded="lg"
-      >
-        <template #title>
-          <v-card-title class="text-wrap">{{ cart.nome }}</v-card-title>
-        </template>
-
-        <v-list-item
-          density="comfortable"
-          prepend-icon="mdi-cash"
-          :title="WDFormatters.formatCurrency(cart.total || 0)"
-        />
-        <v-list-item
-          density="comfortable"
-          prepend-icon="mdi-package-variant-closed"
-          :title="`${cart.produtos.length} ${WDStrings.pluralizeWord('produto', 'produtos', cart.produtos.length)}`"
-        />
-        <v-list-item
-          density="comfortable"
-          prepend-icon="mdi-store-outline"
-          :title="`Colcci`"
-        />
-        <v-list-item
-          density="comfortable"
-          prepend-icon="mdi-identifier"
-          :title="'#' + cart.id"
-        />
-      </v-card>
+      <cart-item-card v-for="cart in list" :key="cart.id" :model-value="cart" />
     </div>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
-import { WDDates, WDFormatters, WDStrings } from 'widelab-utils';
 import { CartService } from '~/services/cart.service';
 
 const configStore = useConfigStore();
