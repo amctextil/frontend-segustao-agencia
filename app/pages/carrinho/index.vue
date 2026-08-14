e01361316fddfc10652f116dec17c13866d8b4bf
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-14 10:34:02 -0300

    feat(Carrinho): incluído campo para nome do carrinho Jira: AMC-417

--- app/pages/carrinho/index.vue
+++ app/pages/carrinho/index.vue
@@ -1,50 +1,55 @@
 <template>
   <NuxtLayout name="main">
-    <h1 class="mx-16 mt-0 align-self-center">Produtos no carrinho</h1>
+    <div v-if="isLoading" class="align-center justify-center d-flex flex-fill">
+      <v-progress-circular indeterminate size="100" />
+    </div>
+    <div v-else class="d-flex flex-column px-16 pt-4">
+      <v-text-field v-model="cartName" label="Nome do carrinho" />
 
-    <div
-      class="px-16 ga-16 flex-fill align-start d-flex flex-row align-self-center overflow-hidden"
-      style="max-width: 1366px"
-    >
-      <ul class="ga-4 d-flex flex-column overflow-auto h-100 pb-16">
-        <div v-if="!cartStore.items.length">
-          <h1>Carrinho vazio!</h1>
+      <h2 class="mx-16 mt-0">Produtos no carrinho</h2>
+
+      <div
+        class="px-16 ga-16 flex-fill align-start d-flex flex-row align-self-center overflow-hidden"
+        style="max-width: 1366px"
+      >
+        <ul class="ga-4 d-flex flex-column overflow-auto h-100 pb-16">
+          <h2 v-if="!cartStore.items.length">Carrinho vazio!</h2>
+
+          <CartProductItem
+            v-for="item in cartStore.items"
+            v-else
+            :key="item.produtoId"
+            :model-value="item"
+          />
+        </ul>
+
+        <div class="d-flex flex-column ga-8">
+          <v-card
+            style="min-width: 320px"
+            title="Subtotal"
+            append-icon="mdi-cash-multiple"
+            variant="tonal"
+          >
+            <div class="ga-8 pa-4 d-flex flex-column">
+              <span class="text-title-large">
+                {{ WDFormatters.formatCurrency(cartStore.totalPrice || 0) }}
+              </span>
+            </div>
+          </v-card>
+
+          <v-btn
+            v-if="cartStore.items.length"
+            prepend-icon="mdi-check"
+            color="success"
+            @click="createCart"
+          >
+            Finalizar carrinho
+          </v-btn>
+
+          <v-btn prepend-icon="mdi-plus" variant="outlined" to="/">
+            Incluir mais produtos
+          </v-btn>
         </div>
-
-        <CartProductItem
-          v-for="item in cartStore.items"
-          v-else
-          :key="item.produtoId"
-          :model-value="item"
-        />
-      </ul>
-
-      <div class="d-flex flex-column ga-8">
-        <v-card
-          style="min-width: 320px"
-          title="Subtotal"
-          append-icon="mdi-cash-multiple"
-          variant="tonal"
-        >
-          <div class="ga-8 pa-4 d-flex flex-column">
-            <span class="text-title-large">
-              {{ WDFormatters.formatCurrency(cartStore.totalPrice || 0) }}
-            </span>
-          </div>
-        </v-card>
-
-        <v-btn
-          v-if="cartStore.items.length"
-          prepend-icon="mdi-check"
-          color="success"
-          @click="createCart"
-        >
-          Finalizar carrinho
-        </v-btn>
-
-        <v-btn prepend-icon="mdi-plus" variant="outlined" to="/">
-          Incluir mais produtos
-        </v-btn>
       </div>
     </div>
 
@@ -75,6 +80,7 @@ if (!config.brand?.data) {
 
 const messages = ref<SnackbarMessage[]>([]);
 const cartName = ref('');
+const isLoading = ref(false);
 
 const createCart = async () => {
   const cartCreated = await CartService.createLink(
