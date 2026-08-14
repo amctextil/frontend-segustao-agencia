3f4bb7fb1b20276b04952742369e501be93a9708
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-14 10:16:30 -0300

    fix(Carrinhos): ajuste no loading dos carirnhos Jira: AMC-417

--- app/pages/carrinho/cadastrados/index.vue
+++ app/pages/carrinho/cadastrados/index.vue
@@ -1,8 +1,17 @@
 <template>
   <NuxtLayout name="main">
-    <div class="d-flex flex-row flex-wrap ga-4 pa-4 overflow-auto">
+    <div
+      v-if="isLoading"
+      class="d-flex flex-row flex-wrap ga-4 pa-4 overflow-hidden"
+    >
+      <v-skeleton-loader v-for="n in 40" :key="n" width="250" height="200" />
+    </div>
+    <div v-else-if="!list.length" class="pa-4">
+      <h2>Não há nenhum carrinho cadastrado para esta marca</h2>
+    </div>
+    <div v-else class="d-flex flex-row flex-wrap ga-4 pa-4 overflow-auto">
       <v-card
-        v-for="cart in cartList"
+        v-for="cart in list"
         :key="cart.id"
         width="250"
         :subtitle="WDDates.toString(new Date(cart.criadoEm), 'BR', true)"
@@ -42,11 +51,27 @@
 import { WDDates, WDFormatters, WDStrings } from 'widelab-utils';
 import { CartService } from '~/services/cart.service';
 
-const { brand } = useConfigStore();
+const configStore = useConfigStore();
 
-const cartList = await CartService.list(brand.appId);
+const list = ref(await CartService.list(configStore.brand.appId));
+const isLoading = ref(false);
 
-cartList.sort((a, b) => b.criadoEm.localeCompare(a.criadoEm));
+list.value.sort((a, b) => b.criadoEm.localeCompare(a.criadoEm));
+
+watch(
+  () => configStore.brand.appId,
+  async (appId) => {
+    isLoading.value = true;
+
+    try {
+      list.value = await CartService.list(appId);
+    } catch {
+      list.value = [];
+    } finally {
+      isLoading.value = false;
+    }
+  },
+);
 </script>
 
 <style></style>
