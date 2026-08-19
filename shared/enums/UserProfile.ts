25c332c4ca31772a74ca248e24c91f1b787acad8
Leonardo Noro Widelab <leonardo.noro@widelab.com.br>
2026-08-19 16:33:52 -0300

    fix(Usuários): removido perfil de gerente por enquanto Jira: AMC-417

--- shared/enums/UserProfile.ts
+++ shared/enums/UserProfile.ts
@@ -1,14 +1,14 @@
 export enum UserProfile {
   ADMIN = 1,
   SELLER = 2,
-  MANAGER = 3,
+  // MANAGER = 3,
 }
 
 export const getUserProfileName = (profile: UserProfile) => {
   const profileMap = {
     [UserProfile.ADMIN]: 'Administrador',
     [UserProfile.SELLER]: 'Vendedor',
-    [UserProfile.MANAGER]: 'Gerente',
+    // [UserProfile.MANAGER]: 'Gerente',
   };
 
   return profileMap[profile];
